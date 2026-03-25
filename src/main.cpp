05b335f90d417267113d153b1dc6546273eedf19
Lucas Lin <lucaslin989@gamil.com>
2026-03-24 18:13:34 -0600

    done

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
-
+template<typename T1, typename T2>
 class Vector2D{
     public:
-        double x;
-        double y;
-        Vector2D(double x=0, double y=0):x(x), y(y){}
+        T1 x;
+        T2 y;
+        Vector2D(T1 x=0, T2 y=0):x(x), y(y){}
 
         Vector2D operator+(const Vector2D& other)const{
             return{x+other.x,y+other.y};
